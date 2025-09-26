a88d5e7c1e081a8156e9140f6b9692d42cf5d247
Florencia Figueroa <165704067+fl0rfigueroa@users.noreply.github.com>
2025-09-26 11:38:41 -0300

    feat: agregar vistas dinamicamente

--- app/src/main/java/com/example/a252_am_lessons/MainActivity.java
+++ app/src/main/java/com/example/a252_am_lessons/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,7 +20,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Log.i("testing", "probando msj");
+
+        Log.i("testing", String.valueOf(R.string.log_message));
+        //1 encontrar container
+        LinearLayout container = findViewById(R.id.container);
+
+        //2 crear el text view
+        TextView textView = new TextView(this);
+        textView.setText(R.string.welcome_message);
+
+        //3 crear el button
+        Button button = new Button(this);
+        button.setText(R.string.button);
+
+        //4 agregar los hijos al container
+        container.addView(textView);
+        container.addView(button);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
